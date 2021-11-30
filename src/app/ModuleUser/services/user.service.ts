31261bf504370f85a58c6b018cc87aa813292906
Your Name <you@example.com>
2021-11-30 23:05:49 +0100

    update

--- src/app/ModuleUser/services/user.service.ts
+++ src/app/ModuleUser/services/user.service.ts
@@ -6,6 +6,7 @@ import { UserSubscribe } from '../models/usersubscribe';
 })
 export class UserService {
   users : UserSubscribe [];
+  user: UserSubscribe;
   constructor() {
    
     this.users = [
@@ -25,6 +26,7 @@ export class UserService {
    }
 
    pushUser(user:UserSubscribe){
+   
        this.users.push(user);
    }
 
@@ -34,5 +36,10 @@ export class UserService {
        this.users.splice(index,1);
      }
    }
+
+   consulterUser( username: string): UserSubscribe  {
+    this.user = this.users.find(u =>u.username == username)
+    return this.user;
+    }
 } 
  
--- src/app/ModuleUser/update-admin/update-admin.component.ts
+++ src/app/ModuleUser/update-admin/update-admin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { UserSubscribe } from '../models/usersubscribe';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-update-admin',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-admin.component.css']
 })
 export class UpdateAdminComponent implements OnInit {
-
-  constructor() { }
+  
+  currentUser = new UserSubscribe();
+  myForm: FormGroup;
+  constructor(private activatedRoute: ActivatedRoute, private userService: UserService) { }
 
   ngOnInit(): void {
+
+    this.myForm = new FormGroup({
+      'newusername': new FormControl('',Validators.required),
+      'oldpassword': new FormControl('',Validators.required),
+      'newpassword': new FormControl('',Validators.required),
+      'confnewpass': new FormControl('',Validators.required),
+    
+    })
+
+
+    //console.log(this.activatedRoute.snapshot.params.username);
+    this.currentUser = this.userService.consulterUser(this.activatedRoute.snapshot.params.username);
+    console.log(this.currentUser);
   }
 
 }
